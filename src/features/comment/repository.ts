321daddfe81cd48b4072ccb0a625bcfebffe854b
alextanhongpin <alextan220990@gmail.com>
2022-12-26 23:02:49 +0800

    chore: remove comment features

--- src/features/comment/repository.ts
+++
@@ -1,7 +0,0 @@
-import type { Comment } from "domain/models";
-
-export type CreateParams = Pick<Comment, "body" | "userId" | "postId">;
-
-export interface CommentRepository {
-  create(params: CreateParams): Promise<Comment>;
-}
