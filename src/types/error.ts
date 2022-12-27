82f04f57149642b740d0100166ec6db11c88a449
alextanhongpin <alextan220990@gmail.com>
2022-12-28 01:50:09 +0800

    chore(types): add base errors

--- src/types/error.ts
+++ src/types/error.ts
@@ -13,9 +13,9 @@ export const enum ErrorKind {
 type ErrorCode = string;
 
 export abstract class AppError<T> extends Error {
-  protected kind: ErrorKind = ErrorKind.Unknown;
-  protected code: ErrorCode = "unknown";
-  protected params: T = {} as T;
+  kind: ErrorKind = ErrorKind.Unknown;
+  code: ErrorCode = "unknown";
+  params: T = {} as T;
 
   // Only works if using target ES2022?: ErrorOptions is a built-in type.
   constructor(message: string, options?: ErrorOptions) {
@@ -31,5 +31,66 @@ export abstract class AppError<T> extends Error {
       message: this.message,
       params: this.params,
     };
+  }
+}
+
+export type ForbiddenErrorParams = {
+  userId: string;
+  entity: string;
+  entityId: string;
+};
+
+export class ForbiddenError extends AppError<ForbiddenErrorParams> {
+  constructor(
+    { userId, entity, entityId }: ForbiddenErrorParams,
+    options?: ErrorOptions
+  ) {
+    super("You do not have access to the given resource", options);
+    this.kind = ErrorKind.Forbidden;
+    this.code = "entity.forbidden";
+    this.params = { userId, entity, entityId };
+  }
+}
+
+export class UnauthorizedError extends AppError<string> {
+  constructor(ipAddress: string, options?: ErrorOptions) {
+    super("You need to login to proceed", options);
+    this.kind = ErrorKind.Unauthorized;
+    this.code = "api.unauthorized";
+    this.params = ipAddress;
+  }
+}
+
+export class NotFoundError extends AppError<{ id: unknown; entity: string }> {
+  constructor(entity: string, id: unknown, options?: ErrorOptions) {
+    super(`${entity} not found`, options);
+    this.kind = ErrorKind.NotFound;
+    this.code = "entity.not_found";
+    this.params = { id, entity };
+  }
+}
+
+export class RequiredFieldError extends AppError<string> {
+  constructor(field: string, options?: ErrorOptions) {
+    super(`field "${field}" is required`, options);
+    this.kind = ErrorKind.BadInput;
+    this.code = "field.required";
+    this.params = field;
+  }
+}
+
+export function validateRequiredFields(obj: Record<string, unknown>) {
+  for (const key in obj) {
+    // For string, number or boolean, it must not be  null or undefined.
+    if (obj[key] === null || obj[key] === undefined) {
+      throw new RequiredFieldError(key);
+    }
+
+    // For arrays, the length must not be 0.
+    if (Array.isArray(obj[key]) && !(obj[key] as Array<unknown>).length) {
+      throw new RequiredFieldError(key);
+    }
+
+    // NOTE: Do we need to check for objects? Probably not.
   }
 }
