aab6ba0d10d05bd2f0f81ceb219a6091dbbb5bea
alextanhongpin <alextan220990@gmail.com>
2023-01-01 00:33:58 +0800

    chore(infra): add postgres config and postgres implementation

---
+++ src/infra/config.ts
@@ -0,0 +1,16 @@
+// config.js stores all the environment variables that
+// will be read from the process.env.
+//
+// All environment variables are strings, conversion to other types has to be handled manually.
+//
+// Validation will be performed by the caller using the
+// environment variables.
+
+export const db = {
+  host: process.env.DB_HOST ?? "",
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME ?? "",
+  username: process.env.DB_USER ?? "",
+  password: process.env.DB_PASS ?? "",
+  debug: true,
+};
